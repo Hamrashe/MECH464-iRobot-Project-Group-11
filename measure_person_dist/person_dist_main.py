1475bea2d86a4d529e185ea6993b35c645bdb936
Hamrashe <39202097+Hamrashe@users.noreply.github.com>
2023-03-11 18:37:35 -0500

    measure_person_distance updates
    - Implemented a move-till-this-distance function
    - made a spin-till-person-is-found function
    
    generally just cleaned up and finalized code, once we have the movement functions, measure_person_dist will be pretty much complete

--- measure_person_dist/person_dist_main.py
+++ measure_person_dist/person_dist_main.py
@@ -8,9 +8,10 @@
 # install opencv "pip install opencv-python"
 import cv2
 
+THREE_FT = 91.44 #cm
 # distance from camera to object(face) measured
 # centimeter
-Known_distance = 91.44
+Known_distance = THREE_FT #3ft
 
 # width of face in the real world or Object Plane
 # centimeter
@@ -47,7 +48,7 @@ def Distance_finder(Focal_Length, real_face_width, face_width_in_frame):
 def face_data(image):
 
 	face_width = 0 # making face width to zero
-
+	error = 99
 	# converting color image to gray scale image
 	gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
@@ -62,108 +63,214 @@ def face_data(image):
 		cv2.rectangle(image, (x, y), (x+w, y+h), GREEN, 2)
 		#cv2.rectangle(image, (x, y), ((x+w/2), (y+h/2)), GREEN, 2)
 		#print('x = ' + str(x+w/2) + '\n')
-		print('error = ' + str(x+w/2.0 - 640/2))
+		error = x+w/2.0 - 640/2
+		#print('error = ' + str(error))
 		#cv2.circle(image, (480/2,640/2), radius=0, color=(0, 0, 255), thickness=1)
 
 		# getting face width in the pixels
 		face_width = w
 
 	# return the face width in pixel
-	return face_width
+	return face_width, error
 
 #Takes a photo which is then used as a reference image, the return of this function is the captured frame
 def take_photo():
     cap = cv2.VideoCapture(0) # video capture source camera (Here webcam of laptop) 
     ret, frame = cap.read() # return a single frame in variable `frame`
     
-    while(True):
+	#turn off press y to save feature
+    """
+	while(True):
         cv2.imshow('img1',frame) #display the captured image
         if cv2.waitKey(1) & 0xFF == ord('y'): #save on pressing 'y' 
             #cv2.imwrite('images/c1.png',frame)
             cv2.destroyAllWindows()
             break
+	"""
 
     cap.release()
     return frame
 
+#Orients the robot to face the person
+def search_person():
+	cap = cv2.VideoCapture(0) # video capture source camera (Here webcam of laptop) 
+	while(True):
+		_, frame = cap.read() # return a frame in variable `frame`
+		#cv2.imshow('img1',frame) #display the captured image
+		face_width = face_data(frame)[0]
+		error = face_data(frame)[1]
+		if face_width != 0 and abs(error) < 20:
+			#cv2.imshow('img1',frame) #display the captured image
+			#slowly rotate, perhaps rotation is proportional to angular velocity
+			#rotate(angular speed = pi rad/s)
+			print('person has been found!')
+			return True
+	
+	cap.release()
+	return False
+
+def meas_dist():
+	# Taking reference_image 
+	ref_image = cv2.imread('measure_person_dist\images\image_ref.png')
 
 
-# Taking reference_image 
-ref_image = take_photo()
+	# find the face width(pixels) in the reference_image
+	ref_image_face_width = face_data(ref_image)[0]
 
-# find the face width(pixels) in the reference_image
-ref_image_face_width = face_data(ref_image)
+	# get the focal by calling "Focal_Length_Finder"
+	# face width in reference(pixels),
+	# Known_distance(centimeters),
+	# known_width(centimeters)
+	Focal_length_found = Focal_Length_Finder(
+		Known_distance, Known_width, ref_image_face_width)
 
-# get the focal by calling "Focal_Length_Finder"
-# face width in reference(pixels),
-# Known_distance(centimeters),
-# known_width(centimeters)
-Focal_length_found = Focal_Length_Finder(
-	Known_distance, Known_width, ref_image_face_width)
+	#print(Focal_length_found)
 
-print(Focal_length_found)
+	# show the reference image
+	#cv2.imshow("ref_image", ref_image)
 
-# show the reference image
-cv2.imshow("ref_image", ref_image)
+	# initialize the camera object so that we
+	# can get frame from it
+	cap = cv2.VideoCapture(0)
 
-# initialize the camera object so that we
-# can get frame from it
-cap = cv2.VideoCapture(0)
+	# looping through frame, incoming from
+	# camera/video
+	while True:
 
-# looping through frame, incoming from
-# camera/video
-while True:
+		# reading the frame from camera
+		_, frame = cap.read()
 
-	# reading the frame from camera
-	_, frame = cap.read()
+		# calling face_data function to find
+		# the width of face(pixels) in the frame
+		face_width_in_frame = face_data(frame)[0]
 
-	# calling face_data function to find
-	# the width of face(pixels) in the frame
-	face_width_in_frame = face_data(frame)
+		# check if the face is zero then not
+		# find the distance
+		if face_width_in_frame != 0:
+			
+			# finding the distance by calling function
+			# Distance finder function need
+			# these arguments the Focal_Length,
+			# Known_width(centimeters),
+			# and Known_distance(centimeters)
+			Distance = Distance_finder(
+				Focal_length_found, Known_width, face_width_in_frame)
+			return Distance
+			break
 
-	# check if the face is zero then not
-	# find the distance
-	if face_width_in_frame != 0:
-		
-		# finding the distance by calling function
-		# Distance finder function need
-		# these arguments the Focal_Length,
-		# Known_width(centimeters),
-		# and Known_distance(centimeters)
-		Distance = Distance_finder(
-			Focal_length_found, Known_width, face_width_in_frame)
+			"""
+			# draw line as background of text
+			cv2.line(frame, (30, 30), (230, 30), RED, 32)
+			cv2.line(frame, (30, 30), (230, 30), BLACK, 28)
 
-		# draw line as background of text
-		cv2.line(frame, (30, 30), (230, 30), RED, 32)
-		cv2.line(frame, (30, 30), (230, 30), BLACK, 28)
+			# Drawing Text on the screen
+			cv2.putText(
+				frame, f"Distance: {round(Distance,2)} CM", (30, 35),
+			fonts, 0.6, GREEN, 2)
+			"""
 
-		# Drawing Text on the screen
-		cv2.putText(
-			frame, f"Distance: {round(Distance,2)} CM", (30, 35),
-		fonts, 0.6, GREEN, 2)
+		# show the frame on the screen
+		#cv2.imshow("frame", frame)
 
-	# show the frame on the screen
-	cv2.imshow("frame", frame)
+		"""
+		# quit the program if you press 'q' on keyboard
+		if cv2.waitKey(1) == ord("q"):
+			break
+		"""
 
-	# quit the program if you press 'q' on keyboard
-	if cv2.waitKey(1) == ord("q"):
-		break
+	# closing the camera
+	#cap.release()
 
-# closing the camera
-cap.release()
+	# closing the windows that are opened
+	#cv2.destroyAllWindows()
+	
 
-# closing the windows that are opened
-cv2.destroyAllWindows()
 
+########OUTPUT FUNCTIONS######################################################################################################################
 
 #Working Function 1: Getting Reference Image
-	#Step 1. drive forward  3ft
+def calibrate_camera():
+	#Step 1. drive forward  3ft 
+	#driveForward(dist = 3ft)
+
 	#Step 2. Look for person (center them in frame) (search_center_target())
-	#Step 3. Take a photo of them
-	#Step 4. Save it as a reference image
+	search_person() #the robot is now facing the target
+		
+	#Step 3. Take a photo of them 
+	ref_image = take_photo()
+
+	#Step 4. write it as the reference image
+	cv2.imwrite('measure_person_dist\images\image_ref.png', ref_image)
+
+	return 0
+
 
 #Working Function 2: Detect distance and move closer 
-	#Step 1. turn untill you see the person and center them (search_center_target())
-	#Step 2. Measure distance and move forward untill you're within the correct distance
+def maintain_dist():
+	#Step 1. turn until you see the person and center them (search_center_target())
+	search_person()
+
+
+	#Step 2. Measure distance and move forward until you're within the correct distance
+	ref_image = cv2.imread('measure_person_dist\images\image_ref.png')
+
+	# find the face width(pixels) in the reference_image
+	ref_image_face_width = face_data(ref_image)[0]
+
+	Focal_length_found = Focal_Length_Finder(
+		Known_distance, Known_width, ref_image_face_width)
+
+	print('focal length is:' + str(Focal_length_found))
+	cap = cv2.VideoCapture(0)
+
+
+	while True:
+
+		# reading the frame from camera
+		_, frame = cap.read()
+
+		# calling face_data function to find
+		# the width of face(pixels) in the frame
+		face_width_in_frame = face_data(frame)[0]
+
+		# check if the face is zero then not
+		# find the distance
+		if face_width_in_frame != 0:
+			
+			# finding the distance by calling function
+			# Distance finder function need
+			# these arguments the Focal_Length,
+			# Known_width(centimeters),
+			# and Known_distance(centimeters)
+			Distance = Distance_finder(
+				Focal_length_found, Known_width, face_width_in_frame)
+			
+			print(Distance)
+			if Distance <= THREE_FT:
+				print('optimal distance reached')
+				break
+			
+			#forward(dist = 0.5ft, speed = 0.25ft/s)
+
 	
+
+
+
+
+
+	
+
+
+
+
+
+##############WORKING CODE BEGINS############################################################################################################################################
+
+#calibrate_camera()
+
+#maintain_dist()
+
+
+
+
 
---
+++ motion/README.txt
@@ -0,0 +1,5 @@
+This folder is for all functions and scripts related to the motion of the robot.
+
+So far what functions I need are:
+	rotate(ang_speed) #rotates the robot at an angular speed of ang_speed [rad/s]
+	forward(dist, speed) #moves the robot forward a given distance at a certain speed
