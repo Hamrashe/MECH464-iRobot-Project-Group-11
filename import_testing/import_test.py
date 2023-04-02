2fd3858b0b4762d102e4b3ade12110c1afad1cb3
Hamrashe <39202097+Hamrashe@users.noreply.github.com>
2023-04-01 22:39:25 -0400

    import testing
    testing out how to import files into other files in python

---
+++ import_testing/import_test.py
@@ -0,0 +1,7 @@
+import sys
+
+sys.path.append( r'C:\Users\hamra\Desktop\Google Drive\MECH 464\Project\MECH464-iRobot-Project-Group-11\motion')
+import import_test2
+import movement
+
+#mymodule.say_hello()
